6023597642fdb18c7906d2961e4fd8c04f3a034c
JTS <151023119+jhonstons@users.noreply.github.com>
2024-06-08 21:12:31 +0900

    Add files via upload
    Review機能のための更新

--- src/main/java/com/example/samuraitravel/entity/House.java
+++ src/main/java/com/example/samuraitravel/entity/House.java
@@ -6,53 +6,53 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
- 
+
 @Entity
 @Table(name = "houses")
 @Data
 public class House {
-     @Id
-     @GeneratedValue(strategy = GenerationType.IDENTITY)
-     @Column(name = "id")
-     private Integer id;
- 
-     @Column(name = "name")
-     private String name;
- 
-     @Column(name = "image_name")
-     private String imageName;
- 
-     @Column(name = "description")
-     private String description;
- 
-     @Column(name = "price")
-     private Integer price;
- 
-     @Column(name = "capacity")
-     private Integer capacity;
- 
-     @Column(name = "postal_code")
-     private String postalCode;
- 
-     @Column(name = "address")
-     private String address;
- 
-     @Column(name = "phone_number")
-     private String phoneNumber;
- 
-     @Column(name = "created_at", insertable = false, updatable = false)
-     private Timestamp createdAt;
- 
-     @Column(name = "updated_at", insertable = false, updatable = false)
-     private Timestamp updatedAt;
-     
-     @OneToMany(mappedBy = "house", cascade = CascadeType.ALL, orphanRemoval = true)
-     private List<Review> reviews;
-     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    
+    @Column(name = "name")
+    private String name;
+    
+    @Column(name = "image_name")
+    private String imageName;    
+        
+    @Column(name = "description")
+    private String description;    
+    
+    @Column(name = "price")
+    private Integer price;    
+    
+    @Column(name = "capacity")
+    private Integer capacity;     
+        
+    @Column(name = "postal_code")
+    private String postalCode;
+        
+    @Column(name = "address")
+    private String address;
+        
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Timestamp createdAt;
+    
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private Timestamp updatedAt;
+    
+    @OneToMany(mappedBy = "house", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Review> reviews;
 }
--- src/main/java/com/example/samuraitravel/entity/Review.java
+++ src/main/java/com/example/samuraitravel/entity/Review.java
@@ -1,30 +1,49 @@
 package com.example.samuraitravel.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "reviews")
 public class Review {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String userName;
-    private String comment;
-    private int rating;
-
-    @ManyToOne
-    @JoinColumn(name = "houses_id")
-    private House house;
     
-    @ManyToOne
-    @JoinColumn(name = "users_id")
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+	@ManyToOne
+	@JoinColumn(name = "house_Id", nullable = false)
+    private House house;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_Id", nullable = false)
     private User user;
+	
+	@Column(nullable = false)
+	private int score;
+	
+	@Column(nullable = false)
+	private String content;
+		
+	@Column(name = "created_at", nullable = false)
+	private LocalDateTime createdAt;
+	
+	@Column(name = "updated_at", nullable = false)
+	private LocalDateTime updatedAt;
+
+	public String getName() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
 
 }
