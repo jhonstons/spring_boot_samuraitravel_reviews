77d3175d2846ca95cb8fec8dd1fdc06807225046
JTS <151023119+jhonstons@users.noreply.github.com>
2024-06-09 15:34:14 +0900

    Add files via upload
    初期状態に戻します

--- src/main/java/com/example/samuraitravel/entity/House.java
+++ src/main/java/com/example/samuraitravel/entity/House.java
@@ -1,16 +1,12 @@
 package com.example.samuraitravel.entity;
 
 import java.sql.Timestamp;
-import java.util.List;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -51,8 +47,5 @@ public class House {
     private Timestamp createdAt;
     
     @Column(name = "updated_at", insertable = false, updatable = false)
-    private Timestamp updatedAt;
-    
-    @OneToMany(mappedBy = "house", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Review> reviews;
+    private Timestamp updatedAt;    
 }
